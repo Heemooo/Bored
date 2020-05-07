f7a6d753f835dfa78a47b72399586d01a5c2d257
hemo <heemooo@foxmail.com>
2020-05-07 16:16:18 +0800

    增加首页handler

--- src/main/java/com/bored/server/handler/IndexHandler.java
+++ src/main/java/com/bored/server/handler/IndexHandler.java
@@ -1,5 +1,7 @@
 package com.bored.server.handler;
 
+import cn.hutool.extra.servlet.ServletUtil;
+import com.bored.Bored;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
@@ -7,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * 首页处理
@@ -15,6 +18,24 @@ public class IndexHandler extends AbstractHandler {
     @Override
     public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         var uri = request.getRequestURI();
-
+        var env = Bored.of().getEnv();
+        var isIndex = false;
+        if (env.getSiteConfig().getEnableHtmlSuffix()) {
+            isIndex = uri.equals("/index." + env.getSiteConfig().getLayoutSuffix()) || uri.equals("/");
+        } else {
+            isIndex = uri.equals("/") || uri.equals("/index");
+        }
+        if (isIndex) {
+            var template = "index.html";
+            var context = new HashMap<String, Object>() {{
+                put("site", env.getSiteConfig());
+                put("pages", env.getPageMap().values());
+                put("tags", env.getTags());
+                put("categories", env.getCategories());
+            }};
+            var content = env.getJetTemplateHelper().parse(template, context);
+            ServletUtil.write(response, content, "text/html;charset=utf-8");
+            baseRequest.setHandled(true);
+        }
     }
 }
