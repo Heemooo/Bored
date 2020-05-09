a67827bf2c83ad41ce211845d07f5802f4e9db9a
heemooo <heemooo@foxmail.com>
2020-05-09 23:47:10 +0800

    增加Context上下文对象

--- src/main/java/com/bored/core/Context.java
+++ src/main/java/com/bored/core/Context.java
@@ -1,7 +1,35 @@
 package com.bored.core;
 
+import lombok.Data;
+
+import java.util.Date;
+
 /**
- * 页面上下文对象
+ * 当前页面的上下文对象
+ * ${this}
  */
+@Data
 public class Context {
+    /**
+     * 当前页面标题
+     */
+    private String title;
+    /**
+     * 当前页面的url
+     */
+    private String url;
+    /**
+     * 当前页面模板路径
+     */
+    private String type;
+    /**
+     * 当前页面模板名
+     */
+    private String layout;
+    /**
+     * 最近创建文章时间
+     */
+    private Date time;
+
+
 }
