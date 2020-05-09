ee2942833ee863237e839845804215599a455650
heemooo <heemooo@foxmail.com>
2020-05-10 00:05:16 +0800

    HTML对象增加content属性

--- src/main/java/com/bored/core/HTML.java
+++ src/main/java/com/bored/core/HTML.java
@@ -2,16 +2,25 @@ package com.bored.core;
 
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.CharsetUtil;
+import cn.hutool.core.util.StrUtil;
 import com.bored.Bored;
+import lombok.Setter;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Setter
 public class HTML {
     private String url;
     private String fullFilePath;
     private Context context;
+    private String content;
     private Map<String, Object> ctx = new HashMap<>();
+
+    public void setContext(Context context) {
+        this.context = context;
+        ctx.put("this", context);
+    }
 
     private HTML add(String key, Object object) {
         ctx.put(key, object);
@@ -19,10 +28,14 @@ public class HTML {
     }
 
     private String content() {
-        return Bored.of().getEnv().getJetTemplateHelper().parse(context.getTemplatePath(), ctx);
+        content = Bored.of().getEnv().getJetTemplateHelper().parse(context.getTemplatePath(), ctx);
+        return content;
     }
 
     private void out() {
-        FileUtil.writeBytes(this.content().getBytes(CharsetUtil.CHARSET_UTF_8), fullFilePath);
+        if(StrUtil.isEmpty(content)){
+            content = this.content();
+        }
+        FileUtil.writeBytes(this.content.getBytes(CharsetUtil.CHARSET_UTF_8), fullFilePath);
     }
 }
