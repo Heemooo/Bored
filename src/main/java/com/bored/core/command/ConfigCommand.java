d3f9bd547c4eeccd69052f25ed73b71a6af258d3
heemooo <heemooo@foxmail.com>
2020-05-13 23:30:16 +0800

    新增:config命令处理

---
+++ src/main/java/com/bored/core/command/ConfigCommand.java
@@ -0,0 +1,35 @@
+package com.bored.core.command;
+
+import java.util.Deque;
+
+public class ConfigCommand extends Command{
+    @Override
+    public String outHelp() {
+        return null;
+    }
+
+    @Override
+    public String getOptionSyntax() {
+        return null;
+    }
+
+    @Override
+    public void displayOptionUsage() {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public void execute(Deque<String> options) {
+
+    }
+}
