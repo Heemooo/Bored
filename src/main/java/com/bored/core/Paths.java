61ebb3e504e3ba4ebf38bfe4a602473f49ce4d63
hemo <heemooo@foxmail.com>
2020-05-13 12:46:23 +0800

    重构:Environment->Paths 路径

--- src/main/java/com/bored/core/Paths.java
+++ src/main/java/com/bored/core/Paths.java
@@ -1,12 +1,84 @@
 package com.bored.core;
 
+import com.bored.Bored;
+
 import java.io.File;
 import java.util.regex.Matcher;
 
 /**
  * 包含所有路径相关的静态方法
  */
-public class Paths {
+public final class Paths {
+
+    private Paths() {
+    }
+
+    /**
+     * 主题目录
+     */
+    public final static String THEME_PATH;
+    /**
+     * 模板文件目录
+     */
+    public final static String LAYOUT_PATH;
+    /**
+     * 静态文件目录
+     */
+    public final static String STATIC_PATH;
+    /**
+     * 文章目录
+     */
+    public final static String PAGE_PATH;
+    /**
+     * 前
+     */
+    public final static String FRONT_MATTER_PATH;
+    /**
+     * 配置文件路径
+     */
+    public final static String CONFIG_PATH;
+    /**
+     * 文件输出目录
+     */
+    public final static String OUTPUT_PATH;
+    /**
+     * 静态文件输出目录
+     */
+    public final static String OUTPUT_STATIC_PATH;
+
+    static {
+        var root = System.getProperty("user.dir");
+        PAGE_PATH = convertCorrectPath(root + "/content");
+        CONFIG_PATH = convertCorrectPath(root + "/config.toml");
+        THEME_PATH = convertCorrectPath(root + "/themes/" + Bored.env().getSiteConfig().getTheme());
+        OUTPUT_PATH = convertCorrectPath(root + "/public");
+        OUTPUT_STATIC_PATH = convertCorrectPath(root + "/public/static");
+        LAYOUT_PATH = convertCorrectPath(THEME_PATH + "/layouts");
+        STATIC_PATH = convertCorrectPath(THEME_PATH + "/static");
+        FRONT_MATTER_PATH = convertCorrectPath(THEME_PATH + "/front.matter.toml");
+
+    }
+
+    /**
+     * 根据主题名称生成对应的主题路径
+     * @param themeName 主题名
+     * @return 主题路径
+     */
+    public static String theme(String themeName) {
+        var root = System.getProperty("user.dir");
+        return convertCorrectPath(root + "/themes/" + themeName);
+    }
+
+    /**
+     * 根据网站名称生成网站路径
+     * @param siteName 网站名称
+     * @return 网站路径
+     */
+    public static String site(String siteName) {
+        var root = System.getProperty("user.dir");
+        return convertCorrectPath(root + "/" + siteName);
+    }
+
     /**
      * 替换路径中的斜杠为当前系统的斜杠
      * @param path 路径
--- src/main/java/com/bored/model/CompleteEnvironment.java
+++ src/main/java/com/bored/model/CompleteEnvironment.java
@@ -2,41 +2,31 @@ package com.bored.model;
 
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.lang.Console;
+import com.bored.core.Paths;
 import com.bored.core.Site;
 import com.bored.template.JetTemplateHelper;
-import com.bored.util.PathUtil;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class CompleteEnvironment extends Environment {
 
     public CompleteEnvironment() {
-        var siteConfigPath = String.format("%s/config.toml", this.getRoot());
-        if (!FileUtil.exist(siteConfigPath)) {
+        if (!FileUtil.exist(Paths.CONFIG_PATH)) {
             Console.log("Error:Site config.toml not found.");
             Console.log("Error:Maybe should create new site or change directory to the site path.");
             System.exit(0);
         }
-        this.setSiteConfigPath(siteConfigPath);
         try {
-            var site = Site.load(siteConfigPath);
+            var site = Site.load(Paths.CONFIG_PATH);
             this.setSiteConfig(site);
         } catch (Exception e) {
             System.exit(0);
         }
-        this.setPagePath(PathUtil.convertCorrectPath(String.format("%s/content", this.getRoot())));
-        this.setThemePath(PathUtil.convertCorrectPath(String.format("%s/themes/%s", this.getRoot(), this.getSiteConfig().getTheme())));
-        this.setLayoutPath(PathUtil.convertCorrectPath(String.format("%s/layouts", this.getThemePath())));
-        this.setOutputPath(PathUtil.convertCorrectPath(String.format("%s/public", this.getRoot())));
-        this.setOutputStaticPath(PathUtil.convertCorrectPath(String.format("%s/static", this.getOutputPath())));
-        this.setJetTemplateHelper(new JetTemplateHelper(this.getLayoutPath()));
-        jetTemplateConfig(this.getSiteConfig());
-
-        this.setFrontMatterPath(PathUtil.convertCorrectPath(String.format("%s/front.matter.toml", this.getThemePath())));
-        this.setStaticPath(PathUtil.convertCorrectPath(String.format("%s/static", this.getThemePath())));
+        this.setJetTemplateHelper(new JetTemplateHelper(Paths.LAYOUT_PATH));
+        jetTemplateConfig(Site.class, "site", this.getSiteConfig());
     }
 
-    public void jetTemplateConfig(Site site) {
-        this.getJetTemplateHelper().getEngine().getGlobalContext().set(Site.class, "site", site);
+    public void jetTemplateConfig(Class<?> clazz, String name, Object object) {
+        this.getJetTemplateHelper().getEngine().getGlobalContext().set(clazz, name, object);
     }
 }
